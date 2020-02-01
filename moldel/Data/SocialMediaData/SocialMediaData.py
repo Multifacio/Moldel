f6151042063e016b923ecd5ddb6b2be85a1b4315
Multifacio <Mfprojects@live.nl>
2020-02-01 19:01:33 +0100

    Added Social Media Data

--- moldel/Data/SocialMediaData/SocialMediaData.py
+++ moldel/Data/SocialMediaData/SocialMediaData.py
@@ -11,7 +11,12 @@ from .SuspicionLevel import *
 # with a tool and determine if that player was active.
 
 # Suspicion Levels are quite subjective, but these are the guidelines for the Suspicion Levels:
-# -NOT_UNLIKELY: Only if you cannot find any form of activity during the recording period.
+# -VERY_LIKELY: If very reliable pictures have leaked of which is it clearly visible that the player participates in
+# the final episode.
+# -LIKELY: If multiple pictures or a picture with all players of that episode multiple is leaked that the player is
+# is present during a later episode of which it is clear that it is not one of the first episodes.
+# -SLIGHTLY_LIKELY: If a picture or other information is leaked that the player is present during a later episode.
+# -NEUTRAL: Only if you cannot find any form of activity during the recording period.
 # -SLIGHTLY_UNLIKELY: If the player has posted more tweets or facebook posts than before. Or had a phone call during
 # night time (of the recorded country). Etcetera...
 # -UNLIKELY: If the player has posted many tweets or facebook post. Has uploaded some instagram pictures. If the
@@ -24,50 +29,51 @@ from .SuspicionLevel import *
 # the player was present during multiple dutch radio recording. Extremely many tweets or facebook posts or a 
 # combination is not enough the be classified in this category.
 
-# Closer to the end date of the recording will be considered closer to NOT_UNLIKELY. Declaration of witnesses will not
+# Activity closer to the end date of the recording will be considered NEUTRAL. Declaration of witnesses will not
 # be used to determine suspicion levels and also rumours of suspicions of any kind will also not be used to determine
-# suspicion levels. If there is no other form of activity then the player has to be classified as NOT_UNLIKELY.
+# suspicion levels. If there is no other form of activity or indications that the player appears in later episodes
+# then the player has to be classified as NEUTRAL.
 
 # Only based on https://www.ad.nl/show/de-social-media-analyse-van-wie-is-de-mol-2019~ac68622f/
 SEASON19 = {Player.ROBERT_19: SuspicionLevel.UNLIKELY, Player.SINAN_19: SuspicionLevel.UNLIKELY,
             Player.EVI_19: SuspicionLevel.VERY_UNLIKELY, Player.NIKKIE_19: SuspicionLevel.UNLIKELY,
-            Player.EVELIEN_19: SuspicionLevel.NOT_UNLIKELY, Player.JAMIE_19: SuspicionLevel.NOT_UNLIKELY,
-            Player.MEREL_19: SuspicionLevel.NOT_UNLIKELY, Player.NIELS_19: SuspicionLevel.NOT_UNLIKELY,
-            Player.RICK_PAUL_19: SuspicionLevel.NOT_UNLIKELY, Player.SARAH_19: SuspicionLevel.NOT_UNLIKELY}
+            Player.EVELIEN_19: SuspicionLevel.NEUTRAL, Player.JAMIE_19: SuspicionLevel.NEUTRAL,
+            Player.MEREL_19: SuspicionLevel.NEUTRAL, Player.NIELS_19: SuspicionLevel.NEUTRAL,
+            Player.RICK_PAUL_19: SuspicionLevel.NEUTRAL, Player.SARAH_19: SuspicionLevel.NEUTRAL}
 
 # Only based on https://www.ad.nl/show/social-media-analyse-wie-is-de-mol-2018~ac1d7cf8/
-SEASON18 = {Player.BELLA_18: SuspicionLevel.NOT_UNLIKELY, Player.EMILIO_18: SuspicionLevel.NOT_UNLIKELY,
-            Player.JAN_18: SuspicionLevel.NOT_UNLIKELY, Player.JEAN_MARC_18: SuspicionLevel.NOT_UNLIKELY,
-            Player.LOES_18: SuspicionLevel.NOT_UNLIKELY, Player.OLCAY_18: SuspicionLevel.NOT_UNLIKELY,
-            Player.RON_18: SuspicionLevel.NOT_UNLIKELY, Player.RUBEN_18: SuspicionLevel.NOT_UNLIKELY,
-            Player.SIMONE_18: SuspicionLevel.NOT_UNLIKELY, Player.STINE_18: SuspicionLevel.NOT_UNLIKELY}
+SEASON18 = {Player.BELLA_18: SuspicionLevel.NEUTRAL, Player.EMILIO_18: SuspicionLevel.NEUTRAL,
+            Player.JAN_18: SuspicionLevel.NEUTRAL, Player.JEAN_MARC_18: SuspicionLevel.NEUTRAL,
+            Player.LOES_18: SuspicionLevel.NEUTRAL, Player.OLCAY_18: SuspicionLevel.NEUTRAL,
+            Player.RON_18: SuspicionLevel.NEUTRAL, Player.RUBEN_18: SuspicionLevel.NEUTRAL,
+            Player.SIMONE_18: SuspicionLevel.NEUTRAL, Player.STINE_18: SuspicionLevel.NEUTRAL}
 
 # Only based on https://www.ad.nl/tv-en-radio/dit-zijn-de-afvallers-van-wie-is-de-mol-2017~a995a64a/
 SEASON17 = {Player.ROOS_17: SuspicionLevel.UNLIKELY, Player.SIGRID_17: SuspicionLevel.UNLIKELY,
-            Player.VINCENT_17: SuspicionLevel.SLIGHTLY_UNLIKELY, Player.DIEDERIK_17: SuspicionLevel.NOT_UNLIKELY,
-            Player.IMANUELLE_17: SuspicionLevel.NOT_UNLIKELY, Player.JEROEN_17: SuspicionLevel.NOT_UNLIKELY,
-            Player.JOCHEM_17: SuspicionLevel.NOT_UNLIKELY, Player.SANNE_17: SuspicionLevel.NOT_UNLIKELY,
-            Player.THOMAS_17: SuspicionLevel.NOT_UNLIKELY, Player.YVONNE_17: SuspicionLevel.NOT_UNLIKELY}
+            Player.VINCENT_17: SuspicionLevel.SLIGHTLY_UNLIKELY, Player.DIEDERIK_17: SuspicionLevel.NEUTRAL,
+            Player.IMANUELLE_17: SuspicionLevel.NEUTRAL, Player.JEROEN_17: SuspicionLevel.NEUTRAL,
+            Player.JOCHEM_17: SuspicionLevel.NEUTRAL, Player.SANNE_17: SuspicionLevel.NEUTRAL,
+            Player.THOMAS_17: SuspicionLevel.NEUTRAL, Player.YVONNE_17: SuspicionLevel.NEUTRAL}
 
 # Only based on http://www.jaapvanzessen.nl/social-media-analist-blogs/nieuwe-wie-is-de-mol-social-media-analyse-live/
 SEASON16 = {Player.AIREN_16: SuspicionLevel.UNLIKELY, Player.TAEKE_16: SuspicionLevel.UNLIKELY,
             Player.ELLIE_16: SuspicionLevel.SLIGHTLY_UNLIKELY, Player.MARJOLEIN_16: SuspicionLevel.SLIGHTLY_UNLIKELY,
-            Player.CECILE_16: SuspicionLevel.SLIGHTLY_UNLIKELY, Player.ANNEMIEKE_16: SuspicionLevel.NOT_UNLIKELY,
-            Player.ROP_16: SuspicionLevel.NOT_UNLIKELY, Player.KLAAS_16: SuspicionLevel.NOT_UNLIKELY,
-            Player.TIM_16: SuspicionLevel.NOT_UNLIKELY, Player.REMY_16: SuspicionLevel.NOT_UNLIKELY}
+            Player.CECILE_16: SuspicionLevel.SLIGHTLY_UNLIKELY, Player.ANNEMIEKE_16: SuspicionLevel.NEUTRAL,
+            Player.ROP_16: SuspicionLevel.NEUTRAL, Player.KLAAS_16: SuspicionLevel.NEUTRAL,
+            Player.TIM_16: SuspicionLevel.NEUTRAL, Player.REMY_16: SuspicionLevel.NEUTRAL}
 
 # Only based on https://www.marketingfacts.nl/berichten/social-media-voorspelt-ook-afvaller-wie-is-de-mol-door-monitoring
 SEASON15 = {Player.CAROLINA_15: SuspicionLevel.SLIGHTLY_UNLIKELY, Player.EVELIEN_15: SuspicionLevel.SLIGHTLY_UNLIKELY,
-            Player.AJOUAD_15: SuspicionLevel.NOT_UNLIKELY, Player.CHRIS_15: SuspicionLevel.NOT_UNLIKELY,
-            Player.MARGRIET_15: SuspicionLevel.NOT_UNLIKELY, Player.MARLIJN_15: SuspicionLevel.NOT_UNLIKELY,
-            Player.MARTINE_15: SuspicionLevel.NOT_UNLIKELY, Player.PIETER_15: SuspicionLevel.NOT_UNLIKELY,
-            Player.RIK_15: SuspicionLevel.NOT_UNLIKELY, Player.VIKTOR_15: SuspicionLevel.NOT_UNLIKELY}
+            Player.AJOUAD_15: SuspicionLevel.NEUTRAL, Player.CHRIS_15: SuspicionLevel.NEUTRAL,
+            Player.MARGRIET_15: SuspicionLevel.NEUTRAL, Player.MARLIJN_15: SuspicionLevel.NEUTRAL,
+            Player.MARTINE_15: SuspicionLevel.NEUTRAL, Player.PIETER_15: SuspicionLevel.NEUTRAL,
+            Player.RIK_15: SuspicionLevel.NEUTRAL, Player.VIKTOR_15: SuspicionLevel.NEUTRAL}
 
 # Only based on http://www.jaapvanzessen.nl/social-media-analist-blogs/wie-de-mol-deelnemer-laten-sporen-achter-op-social-media/
 SEASON14 = {Player.MAURICE_14: SuspicionLevel.UNLIKELY, Player.FREEK_14: SuspicionLevel.SLIGHTLY_UNLIKELY,
-            Player.TYGO_14: SuspicionLevel.UNLIKELY, Player.AAF_14: SuspicionLevel.NOT_UNLIKELY,
-            Player.DAPHNE_14: SuspicionLevel.NOT_UNLIKELY, Player.JAN_WILLEM_14: SuspicionLevel.NOT_UNLIKELY,
-            Player.JENNIFER_14: SuspicionLevel.NOT_UNLIKELY, Player.OWEN_14: SuspicionLevel.NOT_UNLIKELY,
-            Player.SOFIE_14: SuspicionLevel.NOT_UNLIKELY, Player.SUSAN_14: SuspicionLevel.NOT_UNLIKELY}
+            Player.TYGO_14: SuspicionLevel.UNLIKELY, Player.AAF_14: SuspicionLevel.NEUTRAL,
+            Player.DAPHNE_14: SuspicionLevel.NEUTRAL, Player.JAN_WILLEM_14: SuspicionLevel.NEUTRAL,
+            Player.JENNIFER_14: SuspicionLevel.NEUTRAL, Player.OWEN_14: SuspicionLevel.NEUTRAL,
+            Player.SOFIE_14: SuspicionLevel.NEUTRAL, Player.SUSAN_14: SuspicionLevel.NEUTRAL}
 
 SUSPICION_DATA = {19: SEASON19, 18: SEASON18, 17: SEASON17, 16: SEASON16, 15: SEASON15, 14: SEASON14}
--- moldel/Data/SocialMediaData/SuspicionLevel.py
+++ moldel/Data/SocialMediaData/SuspicionLevel.py
@@ -5,7 +5,10 @@ class SuspicionLevel(Enum):
     relative probability decrease of being the mol when a player has this suspicion level. """
     _settings_ = NoAlias
 
-    NOT_UNLIKELY = 1.00
-    SLIGHTLY_UNLIKELY = 0.65
+    VERY_LIKELY = 4.00
+    LIKELY = 2.00
+    SLIGHTLY_LIKELY = 1.35
+    NEUTRAL = 1.00
+    SLIGHTLY_UNLIKELY = 0.50
     UNLIKELY = 0.20
     VERY_UNLIKELY = 0.00
