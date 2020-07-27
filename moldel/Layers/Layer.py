7246d85f700475b0effc8c5833c7e568812f09a5
Multifacio <Mfprojects@live.nl>
2020-07-27 21:42:05 +0200

    Fixed documentation in Layer and simplified EqualLayer

--- moldel/Layers/Layer.py
+++ moldel/Layers/Layer.py
@@ -9,21 +9,21 @@ class Layer:
     def compute_distribution(self, predict_season: int, latest_episode: int, train_seasons: Set[int]) -> Dict[Player, float]:
         """ Compute the likelihood distribution which indicates how likely a player is the 'Mol'.
 
-            Parameters:
-                predict_season (int): The season number for which the likelihood distribution is computed
-                    (the season started at 19 november 1999 is considered as season number 1).
-                latest_episode (int): From the predict_season we only use episode data from episodes with number until
-                    the latest_episode number as observation data. This also includes the entire episode data from the
-                    episode with the latest_episode number. <br>
-                    - Set this value to sys.maxsize if you want to use all episodes from the predict_season as
-                    observation data.
-                    - Set this value to 0 if you want to use no episodes from the predict_season as observation data.
+        Parameters:
+            predict_season (int): The season number for which the likelihood distribution is computed (the season
+                started at 19 november 1999 is considered as season number 1).
+            latest_episode (int): From the predict_season we only use episode data from episodes with numbers until the
+                latest_episode number as observation data. This also includes the entire episode data from the episode
+                with the latest_episode number. <br>
+                - Set this value to sys.maxsize if you want to use all episodes from the predict_season as observation
+                    data.
+                - Set this value to 0 if you want to use no episodes from the predict_season as observation data.
                     (Which can be used to check the performance of only the pre-layers)
-                train_seasons (Set[int]): A set of season number (int) which are used for training this layer.
+            train_seasons (Set[int]): A set of season numbers (int) which are used for training this layer.
 
-            Returns:
-                Dict[Player, float]: A dictionary that contains the likelihood for each player that it is the 'Mol'.
-                The key of this dictionary is the Player for which the likelihood is computed and the value is a float
-                which indicates how likely the player is the 'Mol'.
+        Returns:
+            Dict[Player, float]: A dictionary that contains the likelihood for each player that it is the 'Mol'. The key
+                of this dictionary is the Player for which the likelihood is computed and the value is a float which
+                indicates how likely the player is the 'Mol'.
         """
         pass
--- moldel/Layers/Special/EqualLayer.py
+++ moldel/Layers/Special/EqualLayer.py
@@ -1,14 +1,14 @@
 from Layers.Layer import Layer
 from Data.Player import Player
-from Data.PlayerData import get_season
+from Data.PlayerData import get_players_in_season
 from typing import Dict, Set
 
 class EqualLayer(Layer):
     """ The Equal Layer gives all players an equal likelihood of 1. """
 
     def compute_distribution(self, predict_season: int, latest_episode: int, train_seasons: Set[int]) -> Dict[Player, float]:
-        new_distribution = dict()
-        for player in Player:
-            if get_season(player) == predict_season:
-                new_distribution[player] = 1.0
-        return new_distribution
+        distribution = dict()
+        all_players = get_players_in_season(predict_season)
+        for player in all_players:
+            distribution[player] = 1.0
+        return distribution
