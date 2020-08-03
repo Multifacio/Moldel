66b85ea2f11990ee7d05e5d31ae32ea1ff8de866
Multifacio <Mfprojects@live.nl>
2020-08-03 19:39:56 +0200

    Remove Old Stuff

--- moldel/Old Stuff/ExamLayer/Data/Data.py
+++
@@ -1,9 +0,0 @@
-from Layers.ExamLayer import season13
-from Layers.ExamLayer import season14
-from Layers.ExamLayer import season15
-from Layers.ExamLayer import season16
-from Layers.ExamLayer import season17
-from Layers.ExamLayer import season18
-from Layers.ExamLayer import season19
-
-EXAM_DATA = {13: season13, 14: season14, 15: season15, 16: season16, 17: season17, 18: season18, 19: season19}
