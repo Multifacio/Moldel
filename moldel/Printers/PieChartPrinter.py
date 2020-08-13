7b48c424f91cfca2c1ed95793488d565ebe24a45
Multifacio <Mfprojects@live.nl>
2020-08-13 11:16:55 +0200

    Fixed color and position in piechart for players

--- moldel/Printers/PieChartPrinter.py
+++ moldel/Printers/PieChartPrinter.py
@@ -1,6 +1,9 @@
 from Data.PlayerData import get_name
+from Data.Player import Player
 from Printers.Printer import Printer
+from typing import Dict
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 class PieChartPrinter(Printer):
     """ The Pie Chart Printer prints a pie chart with the likelihoods that players are the Mol. """
@@ -18,16 +21,23 @@ class PieChartPrinter(Printer):
         self.precision = precision
         self.include_threshold = include_threshold
 
-    def print(self, distribution: dict):
-        labels = list()
-        sizes = list()
-        for player, likelihood in distribution.items():
-            likelihood = round(likelihood, self.precision)
+    def print(self, distribution: Dict[Player, float]):
+        palette = sns.color_palette(None, len(distribution))
+        labels = []
+        sizes = []
+        colors = []
+
+        i = 0
+        for player in sorted(distribution.keys(), key=lambda item: item.value):
+            likelihood = round(distribution[player], self.precision)
             if likelihood >= self.include_threshold:
                 labels.append(get_name(player))
                 sizes.append(likelihood)
+                colors.append(palette[i])
+            i += 1
+
         sizes_sum = sum(sizes)
         sizes = [size / sizes_sum for size in sizes]
-        plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+        plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%')
         plt.axis('equal')
         plt.show()
