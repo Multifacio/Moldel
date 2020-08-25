9ea2407cfabc2c061f120a21d550938d6fd0a1e0
Multifacio <Mfprojects@live.nl>
2020-08-25 22:28:00 +0200

    Update the printers

--- moldel/Printers/PieChartPrinter.py
+++ moldel/Printers/PieChartPrinter.py
@@ -1,14 +1,15 @@
 from Data.PlayerData import get_name
 from Data.Player import Player
+from iteround import saferound
 from Printers.Printer import Printer
-from typing import Dict
+from typing import Dict, Union
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 class PieChartPrinter(Printer):
     """ The Pie Chart Printer prints a pie chart with the likelihoods that players are the Mol. """
 
-    def __init__(self, precision: int, include_threshold: float):
+    def __init__(self, precision: int, include_threshold: float, file_name: Union[str, None] = None):
         """ Constructor of the Pie Chart Printer.
 
             Parameters:
@@ -17,9 +18,12 @@ class PieChartPrinter(Printer):
                 include_threshold (float): Only players with a likelihood larger or equal than this threshold will be
                     included in the pie chart (if you do not use this then you can have slice where the text is larger
                     than the slice itself which means that the text can overlap with text from other slices).
+                file_name (Union[str, None]): If set then instead of showing a Pie Chart, it will save a Pie Chart as
+                    this given file name.
         """
-        self.precision = precision
-        self.include_threshold = include_threshold
+        self.__precision = precision
+        self.__include_threshold = include_threshold
+        self.__file_name = file_name
 
     def print(self, distribution: Dict[Player, float]):
         palette = sns.color_palette(None, len(distribution))
@@ -29,15 +33,19 @@ class PieChartPrinter(Printer):
 
         i = 0
         for player in sorted(distribution.keys(), key=lambda item: item.value):
-            likelihood = round(distribution[player], self.precision)
-            if likelihood >= self.include_threshold:
+            likelihood = distribution[player]
+            if likelihood >= self.__include_threshold:
                 labels.append(get_name(player))
                 sizes.append(likelihood)
                 colors.append(palette[i])
             i += 1
 
         sizes_sum = sum(sizes)
-        sizes = [size / sizes_sum for size in sizes]
+        sizes = saferound([size / sizes_sum for size in sizes], self.__precision)
         plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%')
         plt.axis('equal')
-        plt.show()
+        if self.__file_name is None:
+            plt.show()
+        else:
+            plt.savefig(self.__file_name)
+            plt.clf()
