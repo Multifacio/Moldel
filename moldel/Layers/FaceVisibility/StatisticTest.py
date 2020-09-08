cfad04df94ecb704a411b4d0c46d156ba11f7c8d
Multifacio <Mfprojects@live.nl>
2020-09-08 15:56:13 +0200

    Added the Statistic Tester

--- moldel/Layers/FaceVisibility/StatisticTest.py
+++ moldel/Layers/FaceVisibility/StatisticTest.py
@@ -1,12 +1,24 @@
 from Layers.FaceVisibility.FaceVisibilityExtractor import FaceVisibilityExtractor
 from scipy.stats import mannwhitneyu
-
-TEST_SEASONS = {14, 15, 16, 17, 18, 19}
+import matplotlib.pyplot as plt
+import numpy as np
+TEST_SEASONS = {13, 14, 15, 16, 17, 18, 19, 20}
 
 extractor = FaceVisibilityExtractor(0, 0, TEST_SEASONS, 1.0)
 train_input, train_output, _ = extractor.get_train_data()
 non_mol = [data[0] for data, label in zip(train_input, train_output) if label == 0.0]
 mol = [data[0] for data, label in zip(train_input, train_output) if label == 1.0]
 statistics, p_value = mannwhitneyu(non_mol, mol, alternative = "two-sided")
-print(statistics)
-print(p_value)
+print("Score: " + str(statistics))
+print("p-value: " + str(p_value))
+
+train_output += np.random.uniform(-0.1, 0.1, len(train_output))
+print("Number of non-Mol players: " + str(len(non_mol)))
+print("Number of Mol players: " + str(len(mol)))
+plt.figure(figsize=(10, 2))
+plt.scatter(train_input, train_output, s = 4)
+plt.xlabel("Relative Appearance")
+plt.ylabel("Is 'mol'")
+plt.yticks([0.0, 1.0])
+
+plt.show()
