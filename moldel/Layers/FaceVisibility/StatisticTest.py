47ee5031bb4effed6e6a946c89bde326e89bf38e
Multifacio <Mfprojects@live.nl>
2020-09-09 13:33:48 +0200

    Finished Appearance Layer statistic test

--- moldel/Layers/FaceVisibility/StatisticTest.py
+++ moldel/Layers/FaceVisibility/StatisticTest.py
@@ -8,17 +8,18 @@ extractor = FaceVisibilityExtractor(0, 0, TEST_SEASONS, 1.0)
 train_input, train_output, _ = extractor.get_train_data()
 non_mol = [data[0] for data, label in zip(train_input, train_output) if label == 0.0]
 mol = [data[0] for data, label in zip(train_input, train_output) if label == 1.0]
-statistics, p_value = mannwhitneyu(non_mol, mol, alternative = "two-sided")
+statistics, p_value = mannwhitneyu(non_mol, mol, alternative = "greater")
 print("Score: " + str(statistics))
 print("p-value: " + str(p_value))
 
 train_output += np.random.uniform(-0.1, 0.1, len(train_output))
 print("Number of non-Mol players: " + str(len(non_mol)))
 print("Number of Mol players: " + str(len(mol)))
-plt.figure(figsize=(10, 2))
+plt.figure(figsize=(12, 3))
 plt.scatter(train_input, train_output, s = 4)
 plt.xlabel("Relative Appearance")
 plt.ylabel("Is 'mol'")
 plt.yticks([0.0, 1.0])
+plt.gcf().subplots_adjust(bottom = 0.15)
 
 plt.show()
