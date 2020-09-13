64d5bbf935758f73ae586dca97a14ec346c742dd
Multifacio <Mfprojects@live.nl>
2020-09-13 12:32:11 +0200

    Added data and prediction for S21E02

--- moldel/Data/ExamData/Exams/Season21.py
+++ moldel/Data/ExamData/Exams/Season21.py
@@ -45,4 +45,43 @@ episode1 = Episode(players1, result1,
                     Player.ELLIE_21: TestInput({20: 6})},
                    {3: question1_3, 7: question1_7, 13: question1_13, 18: question1_18, 20: question1_20})
 
-season21 = Season(players1, {1: episode1})
+# Aflevering 2 (afvaller: Nadja)
+# Vragen:
+# 1 - De Mol is een:
+# 1: Jeroen, Horace, Patrick, Ron, Tygo; 2: Ellie, Nadja, Nikkie, Peggy;
+# 5 - Tijdens de opdracht 'Steen Rijk' zat de Mol in een:
+# 1: Ellie, Horace, Patrick, Ron; 2: Jeroen, Nadja, Nikkie, Peggy, Tygo;
+# 9 - In welke run betrad de Mol het parcours tijdens de opdracht 'Meeslepend':
+# 1: Nikkie, Ron; 2: Patrick, Peggy; 3: Horace, Tygo; 4: Nadja; 5: Ellie, Jeroen;
+# 16 - Wat was de rol van de Mol tijdens de opdracht 'Vragenvuur':
+# 1: Jeroen, Nadja, Nikkie, Peggy; 2: Ellie, Horace, Ron, Tygo; 3: Patrick;
+# 20 - Wie is de Mol:
+# 1: Ellie; 2: Horace; 3: Jeroen; 4: Nadja; 5: Nikkie; 6: Patrick; 7: Peggy; 8: Ron; 9: Tygo;
+# Antwoorden: Ellie (Vrijstelling), Ron (1, 1), Peggy (9, 4), Horace (5, 2) (1 joker), Nikkie (16, 1), Nadja (1 joker),
+# Tygo (20, 2), Patrick (20, 4) (1 joker)
+players2 = [Player.ELLIE_21, Player.HORACE_21, Player.JEROEN_21, Player.NADJA_21, Player.NIKKIE_21, Player.PATRICK_21,
+            Player.PEGGY_21, Player.RON_21, Player.TYGO_21]
+question2_1 = Question({1: [Player.JEROEN_21, Player.HORACE_21, Player.PATRICK_21, Player.RON_21, Player.TYGO_21],
+                        2: [Player.ELLIE_21, Player.NADJA_21, Player.NIKKIE_21, Player.PEGGY_21]})
+question2_5 = Question({1: [Player.ELLIE_21, Player.HORACE_21, Player.PATRICK_21, Player.RON_21],
+                        2: [Player.JEROEN_21, Player.NADJA_21, Player.NIKKIE_21, Player.PEGGY_21, Player.TYGO_21]})
+question2_9 = Question({1: [Player.NIKKIE_21, Player.RON_21],
+                        2: [Player.PATRICK_21, Player.PEGGY_21],
+                        3: [Player.HORACE_21, Player.TYGO_21],
+                        4: [Player.NADJA_21],
+                        5: [Player.ELLIE_21, Player.JEROEN_21]})
+question2_16 = Question({1: [Player.JEROEN_21, Player.NADJA_21, Player.NIKKIE_21, Player.PEGGY_21],
+                         2: [Player.ELLIE_21, Player.HORACE_21, Player.RON_21, Player.TYGO_21],
+                         3: [Player.PATRICK_21]})
+question2_20 = Question({1: [Player.ELLIE_21], 2: [Player.HORACE_21], 3: [Player.JEROEN_21], 4: [Player.NADJA_21],
+                         5: [Player.NIKKIE_21], 6: [Player.PATRICK_21], 7: [Player.PEGGY_21], 8: [Player.RON_21],
+                         9: [Player.TYGO_21]})
+result2 = Result(DropType.EXECUTION_DROP, [Player.NADJA_21])
+episode2 = Episode(players2, result2,
+                   {Player.ELLIE_21: TestInput(immunity = True), Player.RON_21: TestInput({1: 1}),
+                    Player.PEGGY_21: TestInput({9: 4}), Player.HORACE_21: TestInput({5: 2}, jokers = 1),
+                    Player.NIKKIE_21: TestInput({16: 1}), Player.NADJA_21: TestInput(jokers = 1),
+                    Player.TYGO_21: TestInput({20: 2}), Player.PATRICK_21: TestInput({20: 4}, jokers = 1)},
+                   {1: question2_1, 5: question2_5, 9: question2_9, 16: question2_16, 20: question2_20})
+
+season21 = Season(players1, {1: episode1, 2: episode2})
