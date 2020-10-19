50067411871b7ad3c5c2e55227a5d79f96c938bf
Multifacio <Mfprojects@live.nl>
2020-10-19 15:06:24 +0200

    Improved the Wikipedia Layer implementation

--- moldel/Layers/WikiWord/WikiWordExtractor.py
+++ moldel/Layers/WikiWord/WikiWordExtractor.py
@@ -1,152 +1,94 @@
 from Data.Player import Player
 from Data.PlayerData import get_is_mol
-from Data.WikiWord.Job import Job
-from Layers.WikiWord.WikiWordParser import WikiWordParser
+from Layers.WikiWord.WikiWordParser import WikiWordSample, WikiWordParser
 from numpy.random import RandomState
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 from sklearn.mixture import GaussianMixture
-from typing import Dict, List, NamedTuple, Set, Tuple, Union
-import math
+from sklearn.preprocessing import KBinsDiscretizer
+from typing import Dict, Set, Tuple
 import numpy as np
 
-TrainSample = NamedTuple("TrainSample", [("job_frequency", Union[Dict[Job, float], List[float]]),
-                                         ("number_words", List[float]), ("is_mol", Union[bool, None])])
 class WikiWordExtractor:
-    """ The Wiki Word Extractor excludes and transforms features into a array of floats which can be used by the
+    """ The Wiki Word Extractor transforms an array of features in a new array of features which can be used by the
     classification algorithm. """
 
     # How often a Gaussian Mixture model is tried to fit through the data (from which the best result is taken).
     # Putting this value higher will make the results more stable, however it will decrease the running time.
     GAUSSIAN_MIXTURE_ATTEMPTS = 8
 
-    def __init__(self, predict_season: int, train_seasons: Set[int], pca_components: int, minimum_log: float,
-                 degree_total_count: int, random_generator: RandomState):
+    # Up to which degree the total number of words is polynomial transformed.
+    WORD_COUNT_POLY_DEGREE = 2
+
+    def __init__(self, predict_season: int, train_seasons: Set[int], pca_components: int, random_generator: RandomState):
         """ Constructor of the Wiki Word Extractor.
 
         Arguments:
             predict_season (int): The season for which we make the prediction.
             train_seasons (Set[int]): The seasons which are used as train data.
-            pca_components (int): How many PCA components should be extracted from the job counts as features.
-            minimum_log (float): The lower bound on the job count and total count before applying a logarithm.
-            degree_total_count (int): Up to which degree a polynomial transformation should be applied on the log
-                of the total word count.
+            pca_components (int): The number of PCA components extracted from the job features before LDA is applied.
             random_generator (RandomState): The random generator used to generate random values.
         """
         self.__predict_season = predict_season
         self.__train_seasons = train_seasons
         self.__pca_components = pca_components
-        self.__minimum_log = minimum_log
-        self.__degree_total_count = degree_total_count
         self.__random_generator = random_generator
 
-    def get_train_data(self) -> Tuple[np.array, np.array]:
-        """ Get the formatted train data useable for machine learning algorithms.
+    def train(self):
+        """ Train the Wiki Word Extractor. """
+        raw_train_data = WikiWordParser.parse(self.__train_seasons)
+        train_output = np.array([1.0 if get_is_mol(player) else 0.0 for player in raw_train_data])
+        job_input = np.array([data.job_features for data in raw_train_data.values()])
+        self.__train_job_clusters(job_input)
+        job_input = self.__discretize_jobs(job_input)
+        self.__pca = PCA(n_components = self.__pca_components)
+        job_input = self.__pca.fit_transform(job_input)
+        word_input = np.array([[data.word_feature ** i for i in range(1, self.WORD_COUNT_POLY_DEGREE + 1)]
+                               for data in raw_train_data.values()])
+        train_input = np.concatenate((job_input, word_input), axis = 1)
+        self.__lda = LinearDiscriminantAnalysis()
+        self.__lda.fit(train_input, train_output)
 
-        Returns:
-            A 2d array which represents the train input where each row represents a different train element. And this
-            function also returns a 1d array which represents the train output. The ith row of the train input
-            corresponds to the ith element of the train output.
-        """
-        raw_data = WikiWordParser.parse(self.__train_seasons)
-        train_data = [TrainSample(data.job_frequency, [data.number_words], get_is_mol(player)) for player, data in
-                      raw_data.items()]
-        train_data = [self.__transform_number_words(data) for data in train_data]
-        train_data = [self.__transform_job_frequency(data) for data in train_data]
-        self.__train_job_clusters(train_data)
-        train_data = [self.__discretize_jobs(data) for data in train_data]
-        self.__train_job_pca(train_data)
-        train_data = [self.__apply_job_pca(data) for data in train_data]
-        return np.array([data.job_frequency + data.number_words for data in train_data]), \
-               np.array([1.0 if data.is_mol else 0.0 for data in train_data])
-
-    def get_predict_data(self) -> Dict[Player, List[np.array]]:
+    def get_predict_data(self) -> Dict[Player, np.array]:
         """ Get all formatted predict data useable for the machine learning algorithms to do a prediction.
 
         Returns:
-            A dictionary with as key the players that are still present in the predict episode and as value a list of
-            the predict input for every episode up to (and including) the predict episode.
+            A dictionary with as key the players of that season and as value the formatted predict input.
         """
-        raw_data = WikiWordParser.parse({self.__predict_season})
-        predict_data = {player: TrainSample(data.job_frequency, [data.number_words], None)
-                        for player, data in raw_data.items()}
-        predict_data = {player: self.__transform_number_words(data) for player, data in predict_data.items()}
-        predict_data = {player: self.__transform_job_frequency(data) for player, data in predict_data.items()}
-        predict_data = {player: self.__discretize_jobs(data) for player, data in predict_data.items()}
-        predict_data = {player: self.__apply_job_pca(data) for player, data in predict_data.items()}
-        return {player: np.array([data.job_frequency + data.number_words]) for player, data in predict_data.items()}
+        raw_predict_data = WikiWordParser.parse({self.__predict_season})
+        predict_data = dict()
+        for player, data in raw_predict_data.items():
+            job_input = self.__discretize_jobs(np.array([data.job_features]))
+            job_input = self.__pca.transform(job_input)
+            word_input = np.array([[data.word_feature ** i for i in range(1, self.WORD_COUNT_POLY_DEGREE + 1)]])
+            predict_input = np.concatenate((job_input, word_input), axis = 1)
+            predict_input = self.__lda.transform(predict_input)
+            predict_data[player] = predict_input[0]
+        return predict_data
 
-    def __transform_number_words(self, data: TrainSample) -> TrainSample:
-        """ Transform the number of word feature of a TrainSample using logarithmic transformation and polynomial
-        transformation.
-
-        Arguments:
-            A TrainSample which will be transformed.
-
-        Returns:
-            The transformed TrainSample
-        """
-        total_count = math.log(max(data.number_words[0], self.__minimum_log))
-        poly_transform = [total_count ** i for i in range(1, self.__degree_total_count + 1)]
-        return TrainSample(data.job_frequency, poly_transform, data.is_mol)
-
-    def __transform_job_frequency(self, data: TrainSample) -> TrainSample:
-        """ Transform the job frequencies features of a TrainSample using logarithmic transformation.
-
-        Arguments:
-            A TrainSample which will be transformed.
-
-        Returns:
-            The transformed TrainSample
-        """
-        job_frequencies = {job: math.log(max(frequency, self.__minimum_log)) for job, frequency in
-                           data.job_frequency.items()}
-        return TrainSample(job_frequencies, data.number_words, data.is_mol)
-
-    def __train_job_clusters(self, train_data: List[TrainSample]):
+    def __train_job_clusters(self, job_input: np.array):
         """ Train the Gaussian Mixture clustering for classifying the jobs.
 
         Arguments:
-            All TrainSamples used for training the Gaussian Mixture clustering.
+            job_input (np.array): All job features of all train players.
         """
-        self.__clusters = dict()
-        for job in Job:
-            job_frequencies = np.array([[data.job_frequency[job]] for data in train_data])
+        self.__clusters = []
+        for column in job_input.T:
             cluster = GaussianMixture(n_components = 2, covariance_type = "full", n_init = self.GAUSSIAN_MIXTURE_ATTEMPTS,
                                       random_state = self.__random_generator)
-            cluster.fit(job_frequencies)
-            self.__clusters[job] = cluster
+            cluster.fit(np.expand_dims(column, axis = 1))
+            self.__clusters.append(cluster)
 
-    def __discretize_jobs(self, data: TrainSample) -> TrainSample:
-        """ Discretize the jobs counts of a TrainSample into 0/1 features by checking to which cluster they belong.
+    def __discretize_jobs(self, job_input: np.array) -> np.array:
+        """ Discretize the job input into 0/1 features by checking to which cluster they belong.
 
         Arguments:
-            A TrainSample which will be transformed.
+            job_input (np.array): All job features of all train players.
 
         Returns:
-            The transformed TrainSample
+            The transformed job input.
         """
-        job_frequencies = {job: self.__clusters[job].predict(np.array([[frequency]]))[0] for job, frequency in
-                           data.job_frequency.items()}
-        return TrainSample(job_frequencies, data.number_words, data.is_mol)
-
-    def __train_job_pca(self, train_data: List[TrainSample]):
-        """ Train the principal component analysis used to extract features from the job features.
-
-        Arguments:
-            All TrainSamples used for training the principal component analysis.
-        """
-        all_data = np.array([[data.job_frequency[job] for job in Job] for data in train_data])
-        self.__pca = PCA(n_components = self.__pca_components)
-        self.__pca.fit(all_data)
-
-    def __apply_job_pca(self, data: TrainSample) -> TrainSample:
-        """ Transform the jobs counts of a TrainSample by reducing the features using principal component analysis.
-
-        Arguments:
-            A TrainSample which will be transformed.
-
-        Returns:
-            The transformed TrainSample
-        """
-        job_frequencies = self.__pca.transform(np.array([[data.job_frequency[job] for job in Job]]))[0]
-        return TrainSample(job_frequencies.tolist(), data.number_words, data.is_mol)
+        transformed_input = []
+        for cluster, column in zip(self.__clusters, job_input.T):
+            transformed_input.append(cluster.predict(np.expand_dims(column, axis = 1)))
+        return np.array(transformed_input).T
--- moldel/Layers/WikiWord/WikiWordLayer.py
+++ moldel/Layers/WikiWord/WikiWordLayer.py
@@ -1,58 +1,70 @@
 from Data.Player import Player
 from Data.PlayerData import get_season
-from Data.WikiWord.Linker import LINKER
+from Data.WikiWord.Linker import CHEATING_SEASONS, LINKER
 from Layers.Layer import Layer
 from Layers.Special.EqualLayer import EqualLayer
-from Layers.Special.CutLayer import CutLayer
+from Layers.Special.NormalizeLayer import NormalizeLayer
 from Layers.WikiWord.WikiWordExtractor import WikiWordExtractor
 from numpy.random import RandomState
+from scipy.special import expit
+from scipy import optimize
 from sklearn.linear_model import LogisticRegression
 from statistics import mean
-from typing import Dict, Set
+from typing import Callable, Dict, Set, Tuple
+import math
+import numpy as np
+import scipy as sc
 
 class InnerWikiWordLayer(Layer):
-    MAX_TRAINING_ITERATIONS = 1000 # For how many iterations the logistic regression has to be trained
-
-    def __init__(self, pca_components: int, minimum_log: float, degree_total_count: int, random_generator: RandomState):
+    def __init__(self, unlikely_z_score: float, lower_likelihood: float, pca_components: int, random_generator: RandomState):
+        self.__unlikely_z_score = unlikely_z_score
+        self.__lower_likelihood = lower_likelihood
         self.__pca_components = pca_components
-        self.__minimum_log = minimum_log
-        self.__degree_total_count = degree_total_count
         self.__random_generator = random_generator
 
     def compute_distribution(self, predict_season: int, latest_episode: int, train_seasons: Set[int]) -> Dict[Player, float]:
-        if predict_season not in self.seasons_with_data():
+        available_seasons = self.seasons_with_data()
+        if predict_season not in available_seasons or predict_season in CHEATING_SEASONS:
             return EqualLayer().compute_distribution(predict_season, latest_episode, train_seasons)
+        train_seasons = train_seasons.intersection(available_seasons)
 
-        extractor = WikiWordExtractor(predict_season, train_seasons, self.__pca_components, self.__minimum_log,
-                                      self.__degree_total_count, self.__random_generator)
-        train_input, train_output = extractor.get_train_data()
-        classifier = LogisticRegression(solver = 'lbfgs', max_iter = self.MAX_TRAINING_ITERATIONS)
-        classifier.fit(train_input, train_output)
+        extractor = WikiWordExtractor(predict_season, train_seasons, self.__pca_components, self.__random_generator)
+        extractor.train()
+        return self.__predict(extractor)
 
-        distribution = dict()
-        predict_data = extractor.get_predict_data()
-        for player, data in predict_data.items():
-            distribution[player] = classifier.predict_proba(data)[0][1]
-        return distribution
+    def __predict(self, extractor: WikiWordExtractor) -> Dict[Player, float]:
+        """ Execute the prediction phase of the Wiki Word Layer.
+
+        Arguments:
+            extractor (WikiWordExtractor): The extractor which delivers the prediction data.
+
+        Returns:
+            A dictionary with as key the players that participated in the prediction season and as value the likelihood
+            of being the Mol.
+        """
+        predict_input = extractor.get_predict_data()
+        z_scores = sc.stats.zscore([data for data in predict_input.values()])
+        predict_output = {player: self.__lower_likelihood if z_score < self.__unlikely_z_score else 1.0
+                          for player, z_score in zip(predict_input.keys(), z_scores)}
+        return predict_output
 
     @staticmethod
     def seasons_with_data() -> Set[int]:
         """ Get all seasons that have Wiki Word data. """
         return {get_season(player) for player in LINKER}
 
-class WikiWordLayer(CutLayer):
-    """ The Wiki Word Layer predicts which player is the Mol based on their wikipedia pages. It tries to find the
-     correlation between the number of words on your Wikipage and the likelihood of being the Mol. Furthermore it tries
-     to find a correlation to which job a player belongs and the likelihood of being the Mol. """
+class WikiWordLayer(NormalizeLayer):
+    """ The Wiki Word Layer predicts which player is the Mol based on their Wikipedia pages. It tries to find the
+    correlation between the number of words on your Wikipedia page and the likelihood of being the Mol. Furthermore it
+    tries to find a correlation to which job a player belongs and the likelihood of being the Mol. """
 
-    def __init__(self, pca_components: int, minimum_log: float, degree_total_count: int, random_generator: RandomState):
+    def __init__(self, unlikely_z_score: float, lower_likelihood: float, pca_components: int, random_generator: RandomState):
         """ Constructor of the Wiki Word Layer.
 
         Arguments:
-            pca_components (int): How many PCA components should be extracted from the job counts as features.
-            minimum_log (float): The lower bound on the job count and total count before applying a logarithm.
-            degree_total_count (int): Up to which degree a polynomial transformation should be applied on the log
-                of the total word count.
+            unlikely_z_score (float): All players with a lower z-score than this value get a lower likelihood assigned.
+            lower_likelihood (float): The lower likelihood assigned to these players.
+            pca_components (int): The number of PCA components extracted from the job features before LDA is applied.
             random_generator (RandomState): The random generator used to generate random values.
         """
-        super().__init__(InnerWikiWordLayer(pca_components, minimum_log, degree_total_count, random_generator), mean, 1.0)
+        super().__init__(InnerWikiWordLayer(unlikely_z_score, lower_likelihood, pca_components, random_generator))
