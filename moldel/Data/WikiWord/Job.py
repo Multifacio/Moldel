17b47e37121efce4cd65ab94bc1a4ec11f42159b
Multifacio <Mfprojects@live.nl>
2020-10-19 14:32:28 +0200

    Update Wikipedia Manual Mapping

--- moldel/Data/WikiWord/Job.py
+++ moldel/Data/WikiWord/Job.py
@@ -1,21 +1,22 @@
 from aenum import Enum, NoAlias
 
 class Job(Enum):
-    """ The most common jobs for the players. Each job has a corresponding list of words which belongs to that job."""
+    """ The most common jobs for the players. Each job has a corresponding set of words which belongs to that job."""
     _settings_ = NoAlias
 
-    CABARETIER = ["speelde", "theater", "speelt", "schrijver", "schreef", "spelen", "theaterstuk", "cabaretier", "voorstelling", "stand", "comedytrain"]
-    DJ = ["radio", "top", "3fm", "horen", "tmf", "radioprogramma", "dj", "music"]
-    INSTRUMENTALIST = ["speelde", "top", "theater", "album", "single", "speelt", "muziek", "nummer", "spelen", "tmf", "cd", "band", "theaterstuk", "singles", "hitnotering", "discografie", "music"]
-    JOURNALIST = ["radio", "televisie", "bnn", "rtl", "avro", "omroep", "schrijver", "vara", "kro", "ncrv", "schreef", "verslaggever", "nieuws", "veronica", "vpro", "nos", "at5", "tros", "npo", "journalistiek", "journalist", "rtv"]
-    RADIO_PRESENTATOR = ["radio", "presentator", "presenteerde", "bnn", "rtl", "avro", "top", "omroep", "presenteert", "3fm", "presentatrice", "vara", "kro", "presentatornederlands", "ncrv", "verslaggever", "horen", "presenteren", "nieuws", "veronica", "radioprogramma", "vpro", "nos", "tros", "npo", "music"]
-    RADIO_PRODUCER = ["radio", "bnn", "rtl", "avro", "top", "omroep", "producent", "3fm", "schrijver", "vara", "kro", "ncrv", "schreef", "veronica", "radioprogramma", "vpro", "nos", "tros", "npo", "music"]
-    SCHRIJVER = ["schrijver", "isbn", "boek", "schreef", "columns", "boeken"]
-    SONGWRITER = ["top", "album", "single", "muziek", "nummer", "tmf", "cd", "band", "singles", "hitnotering", "discografie", "music"]
-    STEM_ACTEUR = ["rol", "film", "radio", "televisie", "imdb", "serie", "televisieprogramma", "hoofdrol", "televisieserie", "3fm", "stem", "films", "horen", "hoofdrollen", "rollen", "filmografie", "movie", "gastrol", "voice", "nickelodeon", "vertolkte", "zapp", "gastrollen"]
-    TONEEL_ACTEUR = ["rol", "speelde", "musical", "acteur", "actrice", "theater", "hoofdrol", "speelt", "toneel", "spelen", "hoofdrollen", "rollen", "acteurnederlands", "theaterstuk", "gastrol", "voorstelling", "vertolkte", "gastrollen", "musicals"]
-    TONEEL_PRODUCER = ["theater", "producent", "schrijver", "toneel", "schreef", "theaterstuk", "voorstelling"]
-    TV_ACTEUR = ["rol", "film", "speelde", "televisie", "bnn", "acteur", "rtl", "imdb", "serie", "avro", "televisieprogramma", "actrice", "hoofdrol", "speelt", "televisieserie", "vara", "kro", "films", "aflevering", "ncrv", "spelen", "uitgezonden", "sbs", "hoofdrollen", "rollen", "afl", "filmografie", "acteurnederlands", "movie", "televizier", "gastrol", "nickelodeon", "tros", "npo", "vertolkte", "zapp", "gastrollen", "nps"]
-    TV_PRESENTATOR = ["presentator", "presenteerde", "televisie", "bnn", "rtl", "avro", "televisieprogramma", "omroep", "presenteert", "presentatrice", "vara", "kro", "presentatornederlands", "ncrv", "verslaggever", "tmf", "uitgezonden", "presenteren", "sbs", "nieuws", "veronica", "vpro", "nos", "televizier", "nickelodeon", "at5", "tros", "npo", "rtv", "zapp", "nps"]
-    TV_PRODUCER = ["film", "televisie", "bnn", "rtl", "imdb", "serie", "avro", "televisieprogramma", "omroep", "televisieserie", "producent", "schrijver", "vara", "kro", "films", "ncrv", "schreef", "tmf", "uitgezonden", "sbs", "veronica", "filmografie", "movie", "vpro", "nos", "televizier", "at5", "tros", "npo", "rtv", "zapp", "nps"]
-    ZANGER = ["musical", "top", "theater", "album", "single", "zanger", "muziek", "nummer", "zangeres", "horen", "tmf", "cd", "band", "theaterstuk", "voice", "singles", "hitnotering", "discografie", "music", "musicals"]
+    CABARETIER = {"cabaret", "comedy", "speel", "spel", "theater", "voorstelling"}
+    DJ = {"3fm", "dj", "jockey", "music", "programma", "radio", "tmf", "top"}
+    INSTRUMENTALIST = {"album", "artiest", "cd", "concert", "discografie", "harp", "hitnotering", "lied", "music", "muziek", "nummer", "orkest", "single", "song", "speel", "spel", "theater", "tmf", "top"}
+    JOURNALIST = {"at5", "avro", "bnn", "journaal", "journalist", "krant", "kro", "ncrv", "nieuws", "omroep", "radio", "televisie", "rtl", "vara", "verslag"}
+    RADIO_PRESENTATOR = {"3fm", "avro", "bnn", "journaal", "kro", "music", "ncrv", "nieuws", "omroep", "presentatie", "presentator", "presentatrice", "presenteer", "presenteren", "programma", "radio", "top", "vara", "veronica", "zender"}
+    RADIO_PRODUCER = {"3fm", "avro", "bnn", "kro", "ncrv", "omroep", "produce", "programma", "radio", "schreef", "schrijf", "tekstschrijver", "vara", "veronica", "zender"}
+    SCHRIJVER = {"blad", "boek", "column", "isbn", "krant", "schreef", "schrijf", "schrijver", "strip", "uitgever"}
+    SONGWRITER = {"album", "artiest", "cd", "discografie", "hitnotering", "lied", "music", "muziek", "nummer", "schreef", "schrijf", "single", "song", "songwriter", "tmf", "top"}
+    # SPORTER = {"kampioen", "olympisch", "speel", "spel", "sport", "voetbal", "wk", "zwem"}
+    STEM_ACTEUR = {"animatie", "bijrol", "fabeltjeskrant", "film", "gastrol", "hoofdrol", "imdb", "movie", "nickelodeon", "personage", "programma", "radio", "reclame", "rol", "rollen", "serie", "stem", "stemacteur", "stemactrice", "televisie", "vertolk", "voice"}
+    TONEEL_ACTEUR = {"acteer", "acteur", "actrice", "bijrol", "fabeltjeskrant", "gastrol", "hoofdrol", "musical", "personage", "rol", "rollen", "speel", "spel", "theater", "toneel", "vertolk", "voorstelling"}
+    TONEEL_PRODUCER = {"fabeltjeskrant", "produce", "regisseur", "schreef", "schrijf", "theater", "toneel", "tekstschrijver", "voorstelling"}
+    TV_ACTEUR = {"acteer", "acteur", "actrice", "avro", "baantjer", "bijrol", "bnn", "film", "gastrol", "hoofdrol", "imdb", "kro", "movie", "ncrv", "nickelodeon", "personage", "programma", "reclame", "rol", "rollen", "rtl", "sbs", "sbs6", "serie", "soap", "speel", "spel", "televisie", "televizier", "tv", "uitgezonden", "vara", "vertolk", "zoop", "zwartboek"}
+    TV_PRESENTATOR = {"at5", "avro", "bnn", "journaal", "kro", "ncrv", "nickelodeon", "nieuws", "omroep", "presentatie", "presentator", "presentatrice", "presenteer", "presenteren", "programma", "rtl", "sbs", "sbs6", "televisie", "televizier", "tmf", "tv", "uitgezonden", "vara", "veronica", "zender"}
+    TV_PRODUCER = {"animatie", "at5", "avro", "baantjer", "bnn", "documentaire", "fabeltjeskrant", "film", "imdb", "kro", "movie", "ncrv", "nickelodeon", "omroep", "produce", "programma", "regisseur", "rtl", "sbs", "sbs6", "schreef", "schrijf", "serie", "soap", "tekstschrijver", "televisie", "televizier", "tmf", "tv", "uitgezonden", "vara", "veronica", "zender", "zoop"}
+    ZANGER = {"album", "artiest", "cd", "concert", "discografie", "hitnotering", "lied", "music", "muziek", "nummer", "orkest", "single", "song", "theater", "tmf", "top", "voice", "zang", "zing", "zong"}
