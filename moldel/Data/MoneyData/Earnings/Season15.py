cbc0bee5d620be53a4fb610c2ae6d9c89fe8744b
Multifacio <Mfprojects@live.nl>
2021-02-28 14:34:14 +0100

    Add Some Money Data Season 15

---
+++ moldel/Data/MoneyData/Earnings/Season15.py
@@ -0,0 +1,18 @@
+from Data.MoneyData.Dataclasses.Season import Season
+from Data.MoneyData.Dataclasses.Exercise import Exercise
+from Data.MoneyData.Dataclasses.Earning import Earning
+from Data.Player import Player
+
+# Aflevering 1
+# Opdracht 1 (Maximaal €2000): Niks verdiend
+# Opdracht 2 (Maximaal €2000): Niks verdiend
+alive1 = {Player.AJOUAD_15, Player.CAROLINA_15, Player.CHRIS_15, Player.EVELIEN_15, Player.MARGRIET_15, Player.MARLIJN_15,
+          Player.MARTINE_15, Player.PIETER_15, Player.RIK_15, Player.VIKTOR_15}
+
+# Aflevering 2
+# Opdracht 1 (Maximaal €2000):
+# -€100 (Major: Chris), -€100 (Major: Marlijn), -€100 (Major: Viktor), -€100 (Major: Margriet), -€100 (Major: Marlijn),
+# -€100 (Majo r: Evelien), -€100 (Major: Carolina), -€100 (Major: Chris), €900 (Minor: Allen)
+# Opdracht 2 (Maximaal €1500):
+# €500 (Major: Ajouad, Evelien) (Minor: Carolina), €500 (Major: Margriet, Rik) (Minor: Marlijn),
+# €500 (Major: Chris, Viktor) (Minor: Martine)
