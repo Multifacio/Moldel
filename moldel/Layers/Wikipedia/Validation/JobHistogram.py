7601bee8bafc008954e49f5646ea11eba7894def
Multifacio <Mfprojects@live.nl>
2021-03-05 14:56:53 +0100

    Added Job Histogram script

--- moldel/Layers/Wikipedia/Validation/JobHistogram.py
+++ moldel/Layers/Wikipedia/Validation/JobHistogram.py
@@ -1,22 +1,20 @@
 from Data.Wikipedia.Job import Job
 from Layers.Wikipedia.WikipediaParser import WikipediaParser
-
+import matplotlib.pyplot as plt
+JOB = Job.ZANGER
 SEASONS = {9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21}
 
 dictionary = WikipediaParser.get_standard_dictionary()
-job_occurrences = dict()
+occurrences = []
 all_data = dict()
 for season in SEASONS:
     all_data.update(WikipediaParser.parse_raw(season, dictionary))
 
 for data in all_data.values():
-    for job in Job:
-        count = data.job_counts[job]
-        job_occurrences[job] = job_occurrences.get(job, []) + [count]
+    count = data.job_counts[JOB]
+    occurrences.append(count)
 
-for job, occurrences in job_occurrences.items():
-    print(job)
-    print(sorted(occurrences))
-
-print("Number of words:")
-print(sorted([data.number_words for data in all_data.values()]))
+plt.hist(occurrences, bins = 10, edgecolor = 'black')
+plt.xlabel("Singer Counter")
+plt.ylabel("#Players")
+plt.show()
