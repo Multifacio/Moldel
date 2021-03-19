d608b118cc89c44c3446e0a1b69d25b1dc70b3e8
Multifacio <Mfprojects@live.nl>
2021-03-19 19:47:47 +0100

    Allow Potential Mol Layer to accept no layers

--- moldel/Layers/Special/PotentialMolLayer.py
+++ moldel/Layers/Special/PotentialMolLayer.py
@@ -2,10 +2,14 @@ from Layers.Layer import Layer
 from Layers.Special.CompositeLayer import CompositeLayer
 from Layers.Special.ExamUniformLayer import ExamUniformLayer
 from Layers.Special.ManualExclusionLayer import ManualExclusionLayer
+from typing import Union
 
 class PotentialMolLayer(CompositeLayer):
     """ The Potential Mol layer excludes all players from a Layer that could not be the Mol anymore, which are all
     players that dropped out, seen a red screen or have been revealed to not be the Mol. """
 
-    def __init__(self, layer: Layer):
-        super().__init__([layer, ExamUniformLayer(), ManualExclusionLayer()])
+    def __init__(self, layer: Union[Layer, None] = None):
+        if layer is None:
+            super().__init__([ExamUniformLayer(), ManualExclusionLayer()])
+        else:
+            super().__init__([layer, ExamUniformLayer(), ManualExclusionLayer()])
