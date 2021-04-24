7efdefa8296e6e538045346b3336926e112f5706
Multifacio <Mfprojects@live.nl>
2021-04-24 21:21:19 +0200

    Added function to determine all players alive for the Money Layer.

--- moldel/Data/MoneyData/Dataclasses/Season.py
+++ moldel/Data/MoneyData/Dataclasses/Season.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import List, Set, TYPE_CHECKING
+from ...Player import Player
+from ...PlayerData import get_players_in_season
 
 if TYPE_CHECKING:
     from . import Exercise
@@ -25,3 +27,21 @@ class Season:
         """
         return [exercise for exercise in self.exercises if exercise.episode <= latest_episode and
                 exercise.contains_information()]
+
+    def get_alive(self, latest_episode: float) -> Set[Player]:
+        """ Return all players alive till the latest episode. Return an empty set if there are no exercises.
+
+        Arguments:
+            latest_episode (float): The latest episode.
+
+        Returns:
+            The set of all players alive.
+        """
+        if self.exercises:
+            alive = self.exercises[0].alive.copy()
+            for exercise in self.exercises[1:]:
+                if exercise.episode <= latest_episode:
+                    alive.intersection_update(exercise.alive)
+            return alive
+        else:
+            return set()
