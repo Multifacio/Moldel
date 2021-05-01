df7b31906ea2eabe06eb7249d75dbc078fc0c16d
Multifacio <Mfprojects@live.nl>
2021-05-02 00:51:07 +0200

    Split up Player Data Transfer script with function

--- moldel/DataTransfer/PlayerDataTransfer.py
+++ moldel/DataTransfer/PlayerDataTransfer.py
@@ -1,10 +1,14 @@
 from Data.Player import Player
 from Data.PlayerData import get_name, get_season, get_age, get_is_mol
+from typing import Any, Dict
 import json
 
-FILE_NAME = "data.json"
-data = [{"id": p.value, "name": get_name(p), "season": get_season(p), "is_mol": get_is_mol(p), "age": get_age(p)}
-            for p in Player]
+def json_player(player: Player) -> Dict[str, Any]:
+    return {"id": player.value, "name": get_name(player), "season": get_season(player), "is_mol": get_is_mol(player),
+            "age": get_age(player)}
+
+FILE_NAME = "PlayerData.json"
+data = [json_player(p) for p in Player]
 
 with open("Output/" + FILE_NAME, 'w') as file:
     json.dump(data, file)
