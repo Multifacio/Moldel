07bcf384568514c99e3aa0920aaa42a1926bcd06
Multifacio <Mfprojects@live.nl>
2021-05-03 10:20:45 +0200

    Fix error in Season class

--- moldel/Data/ExamData/Dataclasses/Season.py
+++ moldel/Data/ExamData/Dataclasses/Season.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import Dict, List, Set, TYPE_CHECKING
+from typing import Dict, List, Set, TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     from .Answer import Answer
